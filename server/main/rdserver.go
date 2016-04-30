d4b5417e60479961b1a7e0629257a43ca306e112
angrypie <cookies.in.chocolate@gmail.com>
2016-04-30 22:13:46 +0300

    Start working on 'flags'

--- server/main/rdserver.go
+++ server/main/rdserver.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"github.com/angrypie/remote-desktop/server"
 )
 
+var port = flag.String("port", "9595", "Specify listening port")
+
 func main() {
-	server.Start()
+	server.Start(*port)
 }
--- server/server.go
+++ server/server.go
@@ -1,13 +1,11 @@
 package server
 
 import (
-	"flag"
 	"log"
 )
 
-func Start() {
-	var port = flag.String("port", "9595", "Specify listening port")
+func Start(port string) {
 	log.Println("Server started on port: ", port)
-	wsServerStart(*port)
+	wsServerStart(port)
 	//initDb()
 }
