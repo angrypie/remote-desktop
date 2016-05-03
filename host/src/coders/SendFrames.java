aa6d8a12e8da7bb043770b234528ea7b728d8e61
py3ir <a-danilenko@mail.ru>
2016-05-04 00:19:25 +0300

    change robot screenshot to ffmpeg

--- host/src/coders/SendFrames.java
+++ host/src/coders/SendFrames.java
@@ -12,45 +12,73 @@ import javax.websocket.Session;
 import javax.imageio.ImageIO;
 import javax.websocket.EncodeException;
 import javax.websocket.RemoteEndpoint.Basic;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+
 import coders.Message;
 
 
 public class SendFrames implements Runnable{
 	private int delay=0;
 	private BufferedImage buf;
-	private Robot robot;
-	private Rectangle rect;
-	private BufferedImage mouseCursor;
+	//	private Robot robot;
+	//	private Rectangle rect;
+	//	private BufferedImage mouseCursor;
 	private long start=0;
 	private int count=0;
 	private Basic sync;
 	private volatile boolean startStream=true; 
+	FFmpegFrameGrabber grabber;
+	Java2DFrameConverter converter;
+	Frame frame = null;
 
 	public SendFrames(Session sess,int delay) {
-		try {
-			this.mouseCursor=ImageIO.read(new File("./content/black_cursor.png"));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		//		try {
+		//			this.mouseCursor=ImageIO.read(new File("./content/black_cursor.png"));
+		//		} catch (IOException e1) {
+		//			e1.printStackTrace();
+		//		}
 		sync=sess.getBasicRemote();
 		this.delay=delay;
-		try {
-			robot=new Robot();
-		} catch (AWTException e) {
-			e.printStackTrace();
+		converter=new Java2DFrameConverter();
+		//		try {
+		//			robot=new Robot();
+		//		} catch (AWTException e) {
+		//			e.printStackTrace();
+		//		}
+		//		rect=new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		String os=System.getProperty("os.name");
+		Rectangle rect=new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		if(os.contains("Windows")){
+			grabber = new FFmpegFrameGrabber("video=screen-capture-recorder");
+			grabber.setBitsPerPixel(8);
+			grabber.setImageWidth(rect.width);
+			grabber.setImageHeight(rect.height);
+			grabber.setFormat("dshow");
+			grabber.setFrameRate(30);
 		}
-		rect=new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		else if(os.contains("Linux")){
+			grabber = new FFmpegFrameGrabber(":0.0+" + 0 + "," + 0);
+			grabber.setBitsPerPixel(8);
+			grabber.setImageWidth(rect.width);
+			grabber.setImageHeight(rect.height);
+			grabber.setFormat("x11grab");
+			grabber.setFrameRate(30);
+		}
 	}
 
-	private void showCursor(BufferedImage buf){
-		Graphics2D grfx = buf.createGraphics();
-		PointerInfo p=MouseInfo.getPointerInfo();
-		int mouseX = p.getLocation().x;
-		int mouseY = p.getLocation().y;
-		grfx.drawImage(mouseCursor, mouseX-5, mouseY-2,
-				null);
-		grfx.dispose();
-	}
+	//	private void showCursor(BufferedImage buf){
+	//		Graphics2D grfx = buf.createGraphics();
+	//		PointerInfo p=MouseInfo.getPointerInfo();
+	//		int mouseX = p.getLocation().x;
+	//		int mouseY = p.getLocation().y;
+	//		grfx.drawImage(mouseCursor, mouseX-5, mouseY-2,
+	//				null);
+	//		grfx.dispose();
+	//	}
 
 	public void stopStream(){
 		startStream=false;
@@ -60,14 +88,21 @@ public class SendFrames implements Runnable{
 	@Override
 	public void run() {
 		start=System.currentTimeMillis();
-
+		try {
+			grabber.start();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
 		while(startStream==true){
-			buf= robot.createScreenCapture(rect) ;
-			showCursor(buf);
+			try {
+				frame=grabber.grabFrame();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			buf=converter.getBufferedImage(frame);
 			try {
 				sync.sendObject(new Message("IMG_FRAME",buf));
 			} catch (IOException | EncodeException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			count++;
@@ -81,6 +116,11 @@ public class SendFrames implements Runnable{
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+		try {
+			grabber.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
