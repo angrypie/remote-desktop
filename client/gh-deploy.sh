57378ba753ffa13095c8cef464bf34496e8ae8d8
angrypie <cookies.in.chocolate@gmail.com>
2016-05-11 02:16:05 +0300

    Repair gh-deploy script

--- client/gh-deploy.sh
+++ client/gh-deploy.sh
@@ -3,5 +3,8 @@
 cd client
 NODE_ENV=PRODUCTION webpack
 cp -r dist/* gh-pages/
+git add gh-pages
 cd ..
+git commit -m "Update gh-pages"
 git subtree push --prefix client/gh-pages origin gh-pages
+git push
